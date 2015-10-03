070b2d1a5fe91b260c0c5237396a4daa24cf0d40
Jordanwm <jmoser@mymail.mines.edu>
2015-10-03 13:50:55 -0600

    Added newest version of datatables

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( jquery.dataTables.min.css )
 Rails.application.config.assets.precompile += %w( jquery.dataTables.min.js )
+Rails.application.config.assets.precompile += %w( datatables.min.js )
+Rails.application.config.assets.precompile += %w( datatables.min.css )
