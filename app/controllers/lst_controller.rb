1d9f04f343028bae6e222a0e08b0a64535588c43
iplay88keys <iplay88keys@users.noreply.github.com>
2015-10-03 19:53:13 -0600

    Created list controller

---
+++ app/controllers/lst_controller.rb
@@ -0,0 +1,19 @@
+class LstController < ApplicationController
+  def addToWatchlist
+    if params[:id]
+      
+    end
+
+    if params[:series_id]
+    
+    end
+  end
+
+  def removeFromWatchlist
+    
+  end
+
+  def show
+
+  end
+end
---
+++ app/helpers/lst_helper.rb
@@ -0,0 +1,2 @@
+module LstHelper
+end
