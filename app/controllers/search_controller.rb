a616e0d869fc055b96e2b1b66059b8863e3ae5f8
iplay88keys <iplay88keys@users.noreply.github.com>
2015-10-04 20:57:45 -0600

    Require login for searching

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,15 @@
 require 'tvdb_party'
 
 class SearchController < ApplicationController
+  include MustBeLoggedIn
+  
+  before_action :prevent_access
+   
+  def search
+    #Get the query
+    @query = params[:query]
 
-    def search
-        #Get the query
-        @query = params[:query]
-
-        client = TvdbParty::Search.new(Rails.application.secrets.tvdb_api_key)
-        @results = client.search(@query)
-    end
+    client = TvdbParty::Search.new(Rails.application.secrets.tvdb_api_key)
+    @results = client.search(@query)
+  end
 end
