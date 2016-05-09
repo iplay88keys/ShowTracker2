73c13ba9e249087d80b4666b375c648d8f06e3cc
Jeremy <iplay88keys@users.noreply.github.com>
2016-05-08 22:52:50 -0600

    Update lst_controller.rb

--- app/controllers/api/v1/lst_controller.rb
+++ app/controllers/api/v1/lst_controller.rb
@@ -3,7 +3,7 @@ module Api
     class LstController < BaseController
       include Authenticate
       before_action :restrict_access
-      protect_from_forgery with: :null_session
+      skip_before_filter :verify_authenticity_token
       
       def addToWatchlist
         if Lst.addSeries(params[:id], params[:series_id])
